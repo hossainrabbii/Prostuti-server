792ccb375517d420814248f666487d63048bb71e
Hossain Rabbi <mdhosen21018@gmail.com>
2026-06-25 17:44:24 +0600

    fix mongodb

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,7 @@ import routes from "./app/routes/routes.js";
 import { globalErrorHandler } from "./app/utils/errorHandler.js";
 const app = express();
 const corsOptions = {
-    origin: ["http://localhost:3000"],
+    origin: ["https://shamimvai.vercel.app", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
